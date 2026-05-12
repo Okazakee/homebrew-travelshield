f11e697087cf91f54162a9f6dd141ed8e0b971a0
okazakee <okazakee@proton.me>
2026-05-12 13:51:30 +0200

    Bump formula to v1.1.0

--- Formula/travelshield.rb
+++ Formula/travelshield.rb
@@ -2,8 +2,8 @@
 class Travelshield < Formula
   desc "Arm your disk encryption for the road — toggle TPM2 LUKS auto-unlock on/off with one command"
   homepage "https://github.com/Okazakee/homebrew-travelshield"
-  url "https://github.com/Okazakee/homebrew-travelshield/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "e68006813c2835b772deac9fefb5e94a94be27c6d62fc3b41b1ece7427ca85a9"
+  url "https://github.com/Okazakee/homebrew-travelshield/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "fea9babf3aafe746b0d48a10404f4954c2d9fb87690355f4cd6a3589928ab791"
   license "Unlicense"
 
   def install

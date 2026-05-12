61a9eab14afc0fbac9d4da6eec0ef6acf18867aa
okazakee <okazakee@proton.me>
2026-05-12 13:47:31 +0200

    Remove invalid brew deps, add system package instructions

--- Formula/travelshield.rb
+++ Formula/travelshield.rb
@@ -6,31 +6,23 @@ class Travelshield < Formula
   sha256 "e68006813c2835b772deac9fefb5e94a94be27c6d62fc3b41b1ece7427ca85a9"
   license "Unlicense"
 
-  depends_on "tpm2-tools"
-  depends_on "cryptsetup"
-
   def install
     bin.install "travelshield.sh" => "travelshield"
   end
 
-  def post_install
-    brew_bin = HOMEBREW_PREFIX/"bin"
-    local_bin = Pathname("/usr/local/bin")
-
-    if local_bin.writable? && !(local_bin/"travelshield").exist?
-      local_bin.mkpath
-      FileUtils.ln_s brew_bin/"travelshield", local_bin/"travelshield"
-    end
-  rescue
-    nil
-  end
-
   def caveats
     <<~EOS
-      TravelShield requires systemd-cryptenroll OR clevis-luks to manage TPM2 tokens.
-      Install clevis with: brew install clevis
+      System dependencies required (install before use):
 
-      If the command is not found after install, add Homebrew to your PATH:
+        Debian/Ubuntu:  sudo apt install tpm2-tools cryptsetup-bin
+        Fedora/RHEL:    sudo dnf install tpm2-tools cryptsetup
+        Arch:           sudo pacman -S tpm2-tools cryptsetup
+
+      Additionally, one of these TPM2 LUKS unlock backends is required:
+        - systemd-cryptenroll (built into systemd >= 248)
+        - clevis-luks + tpm2-tools
+
+      If the travelshield command is not found, add Homebrew to your PATH:
         echo 'eval "$(#{HOMEBREW_PREFIX}/bin/brew shellenv)"' >> ~/.bashrc
 
       After toggling travel mode, rebuild your initramfs before rebooting.
